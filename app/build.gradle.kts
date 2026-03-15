5e6c41c2e509d63ce116ed4fc1358c9dac4b865b
NotSash <its.sash024@gmail.com>
2026-03-15 18:49:15 +0530

    Update Gradle build files to replace plugin aliases with direct IDs and adjust Kotlin serialization version for consistency.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,8 +1,8 @@
 plugins {
-    alias(libs.plugins.android.application)
-    alias(libs.plugins.kotlin.compose)
-    id("com.google.dagger.hilt.android") version "2.52"
-    id("org.jetbrains.kotlin.plugin.serialization") version "2.3.10"
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+    id("com.google.dagger.hilt.android")
+    id("org.jetbrains.kotlin.plugin.serialization") version "2.2.10"
     id("org.jetbrains.kotlin.kapt")
 }
 
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    alias(libs.plugins.android.application) apply false
-    alias(libs.plugins.kotlin.compose) apply false
+    id("com.android.application") version "9.1.0" apply false
+    id("org.jetbrains.kotlin.android") version "2.2.10" apply false
+    id("com.google.dagger.hilt.android") version "2.52" apply false
 }
