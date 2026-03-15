0e63d2c999f4340356f7312b8015017e1301821a
NotSash <its.sash024@gmail.com>
2026-03-15 20:06:38 +0530

    Refactor HomeScreen to use collectAsState for uiState and add luminance import in NeumorphicTextField for enhanced functionality.

--- app/src/main/java/com/lockedin/app/core/ui/components/NeumorphicTextField.kt
+++ app/src/main/java/com/lockedin/app/core/ui/components/NeumorphicTextField.kt
@@ -20,6 +20,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.luminance
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.unit.dp
